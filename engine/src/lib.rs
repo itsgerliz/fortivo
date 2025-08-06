50c30de677178b54c76546552cc1d6ba3f2d23ff
itsgerliz <itsgerliz@bonai.es>
2025-08-06 19:13:40 +0200

    chore(engine): add fs module, expose error module

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! This lib is aimed to serve as the engine behind the main Fortivo app\
+//! Nevertheless, it can be used by any other Rust app, and even apps made in other langs with a thin wrapper
+pub mod error;
+mod fs;
