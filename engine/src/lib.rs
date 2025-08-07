8de4dd3f7dad743a89ba9107cd4a7f4532a12102
itsgerliz <itsgerliz@bonai.es>
2025-08-07 20:20:20 +0200

    chore(engine): meaningless comment changes

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,4 +1,8 @@
 //! This lib is aimed to serve as the engine behind the main Fortivo app\
-//! Nevertheless, it can be used by any other Rust app, and even apps made in other langs with a thin wrapper
+//! Nevertheless, it can be used by any other app
+
+// Public modules
 pub mod error;
+
+// Private modules
 mod fs;
