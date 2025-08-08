dda91ba5ee11fdcc3b1402952caf51a3422bfc6a
itsgerliz <itsgerliz@bonai.es>
2025-08-08 13:46:12 +0200

    ft(engine::fs): Make delete_arca() take and drop the Arca instance, make Arca instances hold the path of the file

--- engine/src/fs.rs
+++ engine/src/fs.rs
@@ -1,8 +1,9 @@
-use std::{fs::{self, File}, path::Path};
+use std::{fs::{self, File}, path::{Path, PathBuf}};
 use crate::error::FortivoResult;
 
 pub struct Arca {
 	handle: File,
+	path: PathBuf,
 	is_dirty: bool
 }
 
@@ -10,13 +11,14 @@ pub fn new_arca<P: AsRef<Path>>(pathname: P) -> FortivoResult<Arca> {
 	Ok(
 		Arca {
 			handle: File::create_new(pathname)?,
+			path: pathname.as_ref().to_path_buf(),
 			is_dirty: false
 		}
 	)
 }
 
-pub fn delete_arca<P: AsRef<Path>>(pathname: P) -> FortivoResult<()> {
+pub fn delete_arca(arca: Arca) -> FortivoResult<()> {
 	Ok(
-		fs::remove_file(pathname)?
+		fs::remove_file(arca.path)?
 	)
 }
